f376d3f70a1c411ea52b92708aa8d5d1bdf1f810
Joe <joseph.as.peterson@gmail.com>
2022-07-15 15:30:26 -0600

    fix terraform; deploy working

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,9 @@ module "teams-help-request" {
 
   // required
   name           = "teams-help"
-  image          = "ghcr.io/byuoitav/teams-help-request/teams-help-request"
+  image          = "docker.pkg.github.com/byuoitav/teams-help-request/teams-help-request"
   image_version  = "v0.0.3"
+  container_port = 8080 // doesn't have a port but this is a required field
   repo_url       = "https://github.com/byuoitav/teams-help-request"
 
   // optional
@@ -50,7 +51,7 @@ module "teams-help-request" {
   }
   container_args = [
     "--log-level", "info",
-    "--hub-address", data.aws_ssm_parameter.hub_address.value,
+    "--hub-address", "ws://${data.aws_ssm_parameter.hub_address.value}",
     "--webhook-url", data.aws_ssm_parameter.web_hook.value,
     "--monitoring-url", data.aws_ssm_parameter.av_monitoring.value
   ]
