be24c322735e80e73708c242464ec02fc100b7b1
Jake Peery <jake_peery@yahoo.com>
2025-03-17 09:34:05 -0600

    this is the working code

--- makefile.ps1
+++ makefile.ps1
@@ -3,7 +3,7 @@ $COMMAND = $args[0]
 $NAME = "teams-help-request"
 $OWNER = "byuoitav"
 $PKG = "github.com/$OWNER/$NAME"
-$DOCKER_URL = "docker.pkg.github.com"
+$DOCKER_URL = "ghcr.io"
 $DOCKER_PKG = "$DOCKER_URL/$OWNER/$NAME"
 
 Write-Output "PKG: $PKG"
@@ -11,7 +11,6 @@ Write-Output "DOCKER_PKG: $DOCKER_PKG"
 
 $PRD_TAG_REGEX = "v[0-9]+\.[0-9]+\.[0-9]+"
 $DEV_TAG_REGEX = "v[0-9]+\.[0-9]+\.[0-9]+-.+"
-
 
 $COMMIT_HASH = Invoke-Expression "git rev-parse --short HEAD"
 $TAG = Invoke-Expression "git rev-parse --short HEAD"
@@ -32,7 +31,6 @@ Write-Output "The TAG is: $TAG"
 # go stuff
 $PKG_LIST = Invoke-Expression "go list $PKG/..."
 Write-Output "PKG_LIST: $PKG_LIST"
-
 
 function All {
     Write-Output "All"
@@ -68,20 +66,12 @@ function Build {
     Set-Item -Path env:CGO_ENABLED -Value 0
     Set-Item -Path env:GOOS -Value "linux"
     Set-Item -Path env:GOARCH -Value "amd64"
-    Invoke-Expression "go build -v -o dist/${NAME}-amd64"
-
-    Write-Output "*****************************************"
-    Write-Output "Building for linux-arm"
-    Set-Item -Path env:CGO_ENABLED -Value 0
-    Set-Item -Path env:GOOS -Value "linux"
-    Set-Item -Path env:GOARCH -Value "arm"
-    Invoke-Expression "go build -v -o dist/${NAME}-arm"
+    Invoke-Expression "go build -v -o dist/${NAME}"
 
     Write-Output "Build output is located in ./dist/."
     Set-Item -Path env:GOOS -Value "windows"
     Set-Item -Path env:GOARCH -Value "amd64"
 }
-
 
 function Cleanup {
     Write-Output "Clean"
@@ -97,33 +87,19 @@ function Cleanup {
 function DockerFunc {   #can not just be docker because it creates an infinite loop
     Write-Output "Function Docker      Commit Hash: $COMMIT_HASH     Tag: $TAG"
     if ($COMMIT_HASH -eq $TAG) {
-        Write-Output "Building dev containers with tag $COMMIT_HASH"
-
-        Write-Output "Building container $DOCKER_PKG/$NAME-amd64-dev:$COMMIT_HASH"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-amd64 -t $DOCKER_PKG/$NAME-dev:$COMMIT_HASH dist"
-
-        Write-Output "Building container $DOCKER_PKG/$NAME-arm-dev:$COMMIT_HASH"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-arm -t $DOCKER_PKG/$NAME-arm-dev:$COMMIT_HASH dist"
+        Write-Output "Building dev container with tag $COMMIT_HASH"
+        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME -t $DOCKER_PKG-dev:$COMMIT_HASH dist"
     } elseif ($TAG -match $DEV_TAG_REGEX) {
-        Write-Output "Building dev containers with tag $TAG"
-
-        Write-Output "Building container $DOCKER_PKG/$NAME-amd64-dev:$TAG"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-amd64 -t $DOCKER_PKG/$NAME-dev:$TAG dist"
-
-        Write-Output "Building container $DOCKER_PKG/$NAME-arm-dev:$TAG"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-arm -t $DOCKER_PKG/$NAME-arm-dev:$TAG dist"
+        Write-Output "Building dev container with tag $TAG"
+        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME -t $DOCKER_PKG-dev:$TAG dist"
     } elseif ($TAG -match $PRD_TAG_REGEX) {
-        Write-Output "Building prd containers with tag $TAG"
-
-        Write-Output "Building container $DOCKER_PKG/$NAME-amd64:$TAG"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-amd64 -t $DOCKER_PKG/${NAME}:$TAG dist"
-
-        Write-Output "Building container $DOCKER_PKG/${NAME}-arm:$TAG"
-        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME-arm -t $DOCKER_PKG/${NAME}-arm:$TAG dist"
+        Write-Output "Building prd container with tag $TAG"
+        Invoke-Expression "docker build -f dockerfile --build-arg NAME=$NAME -t ${DOCKER_PKG}:$TAG dist"
     } else {
         Write-Output "Docker function quit unexpectedly. Commit Hash: $COMMIT_HASH     Tag: $TAG"
     }
 }
+
 function Deploy {
     Write-Output "Deploy      Commit Hash: $COMMIT_HASH     Tag: $TAG"
 
@@ -131,34 +107,18 @@ function Deploy {
     Invoke-Expression "docker login $DOCKER_URL -u $Env:DOCKER_USERNAME -p $Env:DOCKER_PASSWORD"
     
     if ($COMMIT_HASH -eq $TAG) {
-        Write-Output "Pushing dev containers with tag $COMMIT_HASH"
-
-        Write-Output "Pushing container $DOCKER_PKG/$NAME-dev:$COMMIT_HASH"
-        Invoke-Expression "docker push $DOCKER_PKG/$NAME-dev:$COMMIT_HASH"
-    
-        Write-Output "Pushing container $DOCKER_PKG/$NAME-arm-dev:$COMMIT_HASH"
-        Invoke-Expression "docker push $DOCKER_PKG/$NAME-arm-dev:$COMMIT_HASH"
+        Write-Output "Pushing dev container with tag $COMMIT_HASH"
+        Invoke-Expression "docker push $DOCKER_PKG-dev:$COMMIT_HASH"
     } elseif ($TAG -match $DEV_TAG_REGEX) {
-        Write-Output "Pushing dev containers with tag $TAG"
-
-        Write-Output "Pushing container $DOCKER_PKG/$NAME-dev:$TAG"
-        Invoke-Expression "docker push $DOCKER_PKG/$NAME-dev:$TAG"
-    
-        Write-Output "Pushing container $DOCKER_PKG/$NAME-arm-dev:$TAG"
-        Invoke-Expression "docker push $DOCKER_PKG/$NAME-arm-dev:$TAG"
+        Write-Output "Pushing dev container with tag $TAG"
+        Invoke-Expression "docker push $DOCKER_PKG-dev:$TAG"
     } elseif ($TAG -match $PRD_TAG_REGEX) {
-        Write-Output "Pushing prd containers with tag $TAG"
-    
-        Write-Output "Pushing container ${DOCKER_PKG}/${NAME}:${TAG}"
-        Invoke-Expression "docker push ${DOCKER_PKG}/${NAME}:${TAG}"
-    
-        Write-Output "Pushing container $DOCKER_PKG/$NAME-arm:$TAG"
-        Invoke-Expression "docker push $DOCKER_PKG/$NAME-arm:$TAG"
+        Write-Output "Pushing prd container with tag $TAG"
+        Invoke-Expression "docker push ${DOCKER_PKG}:$TAG"
     } else {
         Write-Output "Deploy function quit unexpectedly. Commit Hash: $COMMIT_HASH     Tag: $TAG"
     }
 }
-
 
 if ($COMMAND -eq "All") {
     Cleanup
